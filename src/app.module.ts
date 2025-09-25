2f1870329aea0d6bb5cba6f2666b619bf9e2725f
moksy <shmox75@gmail.com>
2025-09-25 16:48:17 +0100

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config'
 
 @Module({
   imports: [    
-    AuthModule
+    AuthModule,
+    ConfigModule.forRoot({
+      load: [appConfig],
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/config/app.config.ts
@@ -0,0 +1,3 @@
+export default () => ({
+    APP_PORT: parseInt(process.env.APP_PORT ?? '3000'),
+});
