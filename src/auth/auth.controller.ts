aa811278f9b29d513b050d03cf1addef8a5f18d6
moksy <shmox75@gmail.com>
2025-09-25 17:00:45 +0100

    ok

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UnauthorizedException, UseGuards, Get, Req } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException, UseGuards, Get, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { LocalStrategy } from './strategies/local.strategy';
@@ -6,6 +6,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { LocalGuard } from './guards/local.guard';
 import { type Request } from 'express'
 import { JwtGuard } from './guards/jwt.guard';
+import { type Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -16,8 +17,17 @@ export class AuthController {
 
     @Post('login')
     @UseGuards(LocalGuard)
-    login(@Req() req: Request) {
-        return req.user;
+    login(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+        const user: any =  req.user;
+
+        res.cookie('jwt', user?.refreshToken, {
+            httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+            secure: false,   // Ensures the cookie is only sent over HTTPS
+            sameSite: 'none', // Controls how cookies are sent with cross-site requests
+            maxAge: 3600000 * 24 * 10, // Cookie expiration time in milliseconds (e.g., 1 hour)
+        });
+
+        return user
     }
 
     @Get('status')
